8a3f62f6f9e977f3aaf67756e41d1774695f0d34
Kresimir Duretec <kresimir.duretec@gmail.com>
2012-09-19 16:24:36 +0200

    initial commit

---
+++ src/main/java/eu/scape_project/simulator/App.java
@@ -0,0 +1,13 @@
+package eu.scape_project.simulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
