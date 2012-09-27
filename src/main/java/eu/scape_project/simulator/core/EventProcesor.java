38cd7761a8c3471353d502676eb8c85044477b59
Kresimir Duretec <kresimir.duretec@gmail.com>
2012-09-28 00:57:30 +0200

    adding core part

---
+++ src/main/java/eu/scape_project/simulator/core/EventProcesor.java
@@ -0,0 +1,10 @@
+package eu.scape_project.simulator.core;
+
+import eu.scape_project.simulator.event.Event;
+
+public class EventProcesor {
+
+	public void addEvent(Event event) {
+		
+	}
+}
---
+++ src/main/java/eu/scape_project/simulator/core/Network.java
@@ -0,0 +1,18 @@
+package eu.scape_project.simulator.core;
+
+import eu.scape_project.simulator.model.Node;
+
+public class Network {
+
+	public void init() {
+		
+	}
+	
+	public void addNode(Node node) {
+		
+	}
+	
+	public void deleteNode(Node node) {
+		
+	}
+}
